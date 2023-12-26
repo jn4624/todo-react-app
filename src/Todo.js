9f7314f3333203cecbc8027a51fd8c1ea4f5b3a1
LimJiNa <jn4624@gmail.com>
2023-12-26 21:19:06 +0900

    todo 삭제 구현

--- src/Todo.js
+++ src/Todo.js
@@ -1,28 +1,51 @@
 import React from "react";
-import { ListItem, ListItemText, InputBase, Checkbox } from "@material-ui/core";
+import {
+    ListItem
+    , ListItemText
+    , InputBase
+    , Checkbox
+    , ListItemSecondaryAction
+    , IconButton
+} from "@material-ui/core";
+import { DeleteOutline } from "@material-ui/icons";
 
 class Todo extends React.Component {
     constructor(props) {
         super(props)
         this.state = { item: props.item };
+        this.delete = props.delete;
+    }
+
+    // delete 함수 추가
+    deleteEventHandler = () => {
+        this.delete(this.state.item);
     }
 
     render() {
         const item = this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done} />
+                <Checkbox checked={item.done} disableRipple />
                 <ListItemText>
                     <InputBase
                         inputProps={{ "aria-label": "naked" }}
                         type="text"
-                        id={item.id}
-                        name={item.id}
+                        id={item.id} // 각 리스트를 구분하려고 id를 연결
+                        name={item.id} // 각 리스트를 구분하려고 id를 연결
                         value={item.title}
                         multiline={true}
                         fullWidth={true}
                     />
                 </ListItemText>
+
+                <ListItemSecondaryAction>
+                    <IconButton
+                        aria-label="Delete Todo"
+                        onClick={this.deleteEventHandler}
+                    >
+                        <DeleteOutline />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
